2e1acd7514b8b15cc13a2afee6613b78b16f5fc8
Yatochi <aqildanial.17@gmail.com>
2024-11-28 13:16:35 +0800

    sec commit

--- main.py
+++ main.py
@@ -8,3 +8,19 @@ class ImgToPDFConv:
         self.root = root
         self.image_paths = []
         self.output_pdf_name = tk.StringVar()
+        self.selected_images_listbox = tk.Listbox(root, selectmode=tk.MULTIPLE)
+
+        self.initialize_ui()
+
+    def initialize_ui(self):
+        title_label = tk.Label(self.root, text = "Image to PDF Converter", font = ("Times New Roman", 20, "bold"))
+        title_label.pack(pady = 10)
+
+def main():
+    root = tk.Tk()
+    root.title("Image to PDF")
+    root.geometry("400x600")
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
